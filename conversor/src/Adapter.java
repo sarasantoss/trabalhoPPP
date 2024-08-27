3ad18ed3bd88fcaafd5eec2fbc2699ca1bd4cdc1
sara <saramiranda201814@gmail.com>
2024-08-27 12:10:03 -0300

    completando

--- conversor/src/Adapter.java
+++ conversor/src/Adapter.java
@@ -1,10 +1,11 @@
 public class Adapter extends Target{
     private Adaptee adaptee;
 
-    public void AdapterTomada(Adaptee tomadaDeTresPinos){
+    public Adapter(Adaptee tomadaDeTresPinos){
         this.adaptee = tomadaDeTresPinos;
     }
 
+    @Override
     public void ligarNaTomadaDeDoisPinos(){
         adaptee.ligarNaTomadaDeTresPinos();
     }
--- conversor/src/App.java
+++ conversor/src/App.java
@@ -1,7 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Adaptee TomadadeTresPinos = new Adaptee();
 
+       Adapter a = new Adapter(TomadadeTresPinos);
+
+       a.ligarNaTomadaDeDoisPinos();
         
     }
 }
